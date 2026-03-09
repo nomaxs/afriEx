d515cc7661d2184bd046372a3ce8698bfb984820
nomaxs <110211638+nomaxs@users.noreply.github.com>
2026-03-09 18:19:48 +0100

    Update generateArchives.js

--- generator/generateArchives.js
+++ generator/generateArchives.js
@@ -7,7 +7,7 @@ const top = require("../data/topCurrencies.json")
 async function run() {
   try {
     // Fetch latest rates
-    const res = await fetch("https://api.exchangerate.host/latest?base=USD")
+    const res = await fetch("https://cdn.moneyconvert.net/api/latest.json")
     const data = await res.json()
 
     // Get today's date components
