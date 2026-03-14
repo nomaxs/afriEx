b13e3a8344c378757e75dcb6d3ce52161b154444
nomaxs <110211638+nomaxs@users.noreply.github.com>
2026-03-14 19:55:12 +0100

    Update sitemapGenerator.js

--- generator/sitemapGenerator.js
+++ generator/sitemapGenerator.js
@@ -3,6 +3,25 @@ const path = require("path")
 
 const african = require("../data/africanCurrencies.json")
 const top = require("../data/topCurrencies.json")
+const SITE_URL = "https://african-exchange.com";
+
+function latestDate() {
+
+const archiveFolder = path.join(__dirname,"..","archives")
+
+const years = fs.readdirSync(archiveFolder)
+const year = years.sort().reverse()[0]
+
+const months = fs.readdirSync(path.join(archiveFolder,year))
+const month = months.sort().reverse()[0]
+
+const days = fs.readdirSync(path.join(archiveFolder,year,month))
+const day = days.sort().reverse()[0]
+
+return `${year}-${month}-${day}`
+
+}
+
 
 // Safe path
 const seoDir = path.join(__dirname, "..", "seo")
@@ -10,13 +29,16 @@ if (!fs.existsSync(seoDir)) fs.mkdirSync(seoDir, { recursive: true })
 
 const urls = []
 
+const date = latestDate()
+const [year,month,day] = date.split("-")
+
 // Generate all combinations: african ↔ african, african ↔ top, top ↔ african, top ↔ top
 const currencies = [...african, ...top]
 
 currencies.forEach(base => {
   currencies.forEach(target => {
     if (base !== target) {
-      urls.push(`/pages/${base}-to-${target}.html`)
+      urls.push(`/pages/${year}/${month}/${day}/${base}-to-${target}-exchange-${date}.html`)
     }
   })
 })
@@ -24,7 +46,7 @@ currencies.forEach(base => {
 let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`
 xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
 urls.forEach(u => {
-  xml += `\n  <url><loc>https://site.com${u}</loc></url>`
+  xml += `\n  <url><loc>${SITE_URL}${u}</loc></url>`
 })
 xml += `\n</urlset>`
 
