0e678a98b6657017ca4a53a2e7e0881457828353
nomaxs <110211638+nomaxs@users.noreply.github.com>
2026-03-17 20:05:08 +0100

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 const overlay = document.getElementById("calculatorOverlay")
-const ads = document.getElementById("adBanner")
-
 const fromSelect = document.getElementById("fromCurrency")
 const toSelect = document.getElementById("toCurrency")
 const amountInput = document.getElementById("amount")
@@ -10,17 +8,13 @@ const resultBox = document.getElementById("result")
 
 document.getElementById("openCalculator").onclick = () => {
 overlay.style.display = "flex"
-ads.style.display = "none"
 }
 
 overlay.onclick = e => {
 if(e.target === overlay){
 overlay.style.display = "none"
-ads.style.display = "flex"
 }
 }
-
-
 
   
 const currencyMeta = {
